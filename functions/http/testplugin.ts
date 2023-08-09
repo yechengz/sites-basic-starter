029b5b924622b220e767731164b175395903a41a
Ye Cheng Zheng <yechengzh@gmail.com>
2023-08-09 14:37:52 -0400

    cache control header

--- functions/http/testplugin.ts
+++ functions/http/testplugin.ts
@@ -2,7 +2,7 @@ export async function main() {
     return {
         statusCode: 200,
         headers: {
-            "test-header": "test",
+            "Cache-Control": "no-cache",
         },
         body: "test"
     }
